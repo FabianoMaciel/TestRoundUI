155835632cbc9827a70509452d43112afa6b7177
Fabiano Marcolin Maciel <fabianommaciel@hotmail.com>
2021-10-21 22:58:49 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using TestRound.Models;
+using TestRoundGLUI.Models;
+
+namespace TestRoundGLUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private HttpClient _client;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _client = new HttpClient();
+            // Update port # in the following line.
+            _client.BaseAddress = new Uri("https://localhost:44358/");
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await _client.GetAsync("company");
+            var companies = JsonConvert.DeserializeObject<IEnumerable<CompanyModel>>(await response.Content.ReadAsStringAsync());
+
+            return View(companies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/CompanyModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestRound.Models
+{
+    [Serializable]
+    public class CompanyModel
+    {
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Exchange")]
+        public string Exchange { get; set; }
+
+        [Display(Name = "Ticker")]
+        public string Ticker { get; set; }
+
+        [Display(Name = "ISIN")]
+        public string ISIN { get; set; }
+        [Display(Name = "WebSitexx")]
+        public string WebSite { get; set; }
+    }
+}
